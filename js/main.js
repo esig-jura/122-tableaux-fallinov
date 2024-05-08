a6ef5f7649da7856825558aafcc3be1aae381ba7
Steve Fallet <steve.fallet@divtec.ch>
2024-05-08 14:44:53 +0200

    ajout personnes et style form

--- js/main.js
+++ js/main.js
@@ -5,4 +5,35 @@
 
 'use strict';
 
-console.log('Test liaison JS');
+const personnes = [
+    {
+        nom: 'Doe',
+        prenom: 'John',
+        age: 25,
+        localite: 'New York',
+    },
+    {
+        nom: 'Doe',
+        prenom: 'Jane',
+        age: 30,
+        localite: 'Los Angeles',
+    },
+    {
+        nom: 'Doe',
+        prenom: 'Jack',
+        age: 28,
+        localite: 'Chicago',
+    },
+    {
+        nom: 'Doe',
+        prenom: 'Jill',
+        age: 35,
+        localite: 'Miami',
+    },
+    {
+        nom: 'Doe',
+        prenom: 'Jim',
+        age: 40,
+        localite: 'San Francisco',
+    }
+];
