f8839e360a0633c06cd2162ed4ad34446bf579bb
Steve Fallet <steve.fallet@divtec.ch>
2024-05-08 16:01:28 +0200

    ajout ligne au tableau

--- js/main.js
+++ js/main.js
@@ -37,3 +37,30 @@ const personnes = [
         localite: 'San Francisco',
     }
 ];
+
+// Récupère le 1er formulaire du document
+const formulaire = document.querySelector('form');
+// Récupère le champ texte #nom
+const txtNom = document.querySelector('#nom');
+const txtPrenom = document.querySelector('#prenom');
+// Récupère le premier corps de tableau <tbody>
+const tableBody = document.querySelector('tbody');
+// Test si la récupération des éléments HTML est OK
+console.log(formulaire, txtNom, tableBody);
+
+// Ecouter l'envoi du formulaire
+formulaire.addEventListener('submit', function envoyer(event) {
+   // Stopper l'envoi du formulaire
+    event.preventDefault();
+    // Crée le code HTML da la nouvelle ligne
+    let nouvelleLigne = `
+        <tr>
+            <td>${ txtPrenom.value }</td>
+            <td>${ txtNom.value }</td>
+            <td></td>
+            <td></td>
+        </tr>
+    `;
+    // Ajouter la nouvelle personne à la fin du <tbody>
+    tableBody.innerHTML += nouvelleLigne;
+});
